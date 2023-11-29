9e24d5f3623749beac661e69c86496800dc55a14
K234k <kaitoueno2@gmail.com>
2023-11-30 01:47:39 +0900

    issue６の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -19,12 +19,13 @@ class CalendarsController < ApplicationController
   end
 
   def getWeek
-    wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-
     @todays_date = Date.today
     @week_days = []
-  
+    @wdays = ['日','月','火','水','木','金','土']
     plans = Plan.where(date: @todays_date..@todays_date + 6)
+  
+    # 曜日を表す配列
+    @wdays = ['日','月','火','水','木','金','土']
   
     7.times do |x|
       today_plans = []
@@ -32,10 +33,14 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
   
-      day_of_week = @todays_date + x
-      day_name = wdays[day_of_week.wday] # 曜日を取得
-  
-      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, day_of_week: day_name }
+      wday_num = (@todays_date + x).wday
+    
+
+      if wday_num >= 7 
+        wday_num = wday_num - 7
+      end
+
+      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wday_num }
       @week_days.push(days)
     end
   end
